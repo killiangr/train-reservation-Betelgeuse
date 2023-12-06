b5e2c6c11c105ce07607041e79a344eb7304afaa
Senhua <senhua.liu@efrei.net>
2023-12-06 11:55:09 +0100

    test 1

--- ticket_office/test_app.py
+++ ticket_office/test_app.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+from function import get_available_seats
+
 
 def test_reserve_seats_from_empty_train():
     train_id = "express_2000"
@@ -40,3 +42,24 @@ def test_reserve_four_additional_seats():
     assert reservation["train_id"] == "express_2000"
     assert len(reservation["seats"]) == 4
     assert reservation["seats"] == ["5A", "6A", "7A", "8A"]
+
+
+def test_get_available_seats():
+    # Mock train_data for testing
+    train_data = {
+        "seats": {
+            "1A": {"seat_number": 1, "coach": "A", "booking_reference": None},
+            "2A": {"seat_number": 2, "coach": "A", "booking_reference": "ABC123"},
+            "3A": {"seat_number": 3, "coach": "A", "booking_reference": None},
+        }
+    }
+
+    result = list(get_available_seats(train_data))
+
+    # Check that only seats without booking_reference are returned
+    expected_result = [
+        {"seat_number": 1, "coach": "A", "booking_reference": None},
+        {"seat_number": 3, "coach": "A", "booking_reference": None},
+    ]
+
+    assert result == expected_result
